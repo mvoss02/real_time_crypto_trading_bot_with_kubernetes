f095c5d789f5e0229ce8ad7c2ba7988114fe7f38
mvoss02 <m_l_voss@web.de>
2025-05-27 16:51:53 +0200

    Added technical technical_indicators service, plus docker image

--- deployments/dev/kind/create_cluster.sh
+++ deployments/dev/kind/create_cluster.sh
@@ -26,10 +26,10 @@ echo "Installing Kafka UI..."
 chmod +x ./install_kafka_ui.sh
 ./install_kafka_ui.sh
 
-# # 7. Install RisingWave
-# echo "Installing RisingWave..."
-# chmod 755 ./install_risingwave.sh
-# ./install_risingwave.sh
+# 7. Install RisingWave
+echo "Installing RisingWave..."
+chmod 755 ./install_risingwave.sh
+./install_risingwave.sh
 
 # # 8. Install Grafana
 # echo "Installing Grafana..."
